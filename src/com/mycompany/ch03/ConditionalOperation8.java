214287158da30994b37366cb5dba0f49e5894963
hyobin-park <qlswkd03@gmail.com>
2024-07-29 13:59:42 +0900

    conditional operation

--- src/com/mycompany/ch03/ConditionalOperation8.java
+++ src/com/mycompany/ch03/ConditionalOperation8.java
@@ -11,6 +11,15 @@ public class ConditionalOperation8 {
 		char grade = (math > 90) ? 'A' : 'B';
 		System.out.println("grade: " + grade);
 		
+		
+//		값이 0인 변수 선언
+//		선언한 변수의 값이 0이면 10 대입 아니면 20 대입
+		
+		int a = 0;
+		
+		int b = (a == 0) ? 10 : 20;
+		System.out.println("b: " + b);
+		
 	}
 
 }
