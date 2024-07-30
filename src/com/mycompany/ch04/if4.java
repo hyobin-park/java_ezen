57241353cfe34b7eea807d31208f3618578ccd96
hyobin-park <qlswkd03@gmail.com>
2024-07-30 12:37:47 +0900

    if, switch new

--- src/com/mycompany/ch04/if4.java
+++ src/com/mycompany/ch04/if4.java
@@ -3,8 +3,47 @@ package com.mycompany.ch04;
 public class if4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+//		int 타입의 변수를 2개 선언하고
+//		두 정수가 주어졌을 때 사분면 어디에 해당되는지 출력하는 프로그램을 작성하시오
+		
+//		System.out.println("(a,b)는 1사분면에 위치합니다.");
+		
+		int a = 10;
+		int b = 15;
+		
+		if(a>0 && b>0) {
+			System.out.println("(10,15)는 1사분면에 위치합니다.");
+		} else if (a<0 && b>0) {
+			System.out.println("(10,15)는 2사분면에 위치합니다.");
+		} else if (a<0 && b<0) {
+			System.out.println("(10,15)는 3사분면에 위치합니다.");
+		} else if (a>0 && b<0){
+			System.out.println("(10,15)는 4사분면에 위치합니다.");
+		}
+		
+		
+	/* 선생님 코딩 - 예외도 꼭 코딩해주기
+	 	int a = -2;
+		int b = 0;
+		
+		if(a>0 && b>0) {
+			System.out.println("(a,b)는 1사분면에 위치합니다");
+		} else if(a<0 && b>0) {
+			System.out.println("(a,b)는 2사분면에 위치합니다");
+		} else if(a<0 && b<0) {
+			System.out.println("(a,b)는 3사분면에 위치합니다");
+		} else if(a>0 && b<0) {
+			System.out.println("(a,b)는 4사분면에 위치합니다");
+		} else if(a==0 && b!= 0) {
+			System.out.println("(a,b)는 x축 위에 있습니다");
+		} else if(a!=0 && b== 0) {
+			System.out.println("(a,b)는 y축 위에 있습니다");
+		} else {
+			// a==0 & b==0
+			System.out.println("(a,b)는 원점입니다.");
+		} */
+		
 	}
 
 }
---
+++ src/com/mycompany/ch04/if5.java
@@ -0,0 +1,40 @@
+package com.mycompany.ch04;
+
+public class if5 {
+
+	public static void main(String[] args) {
+
+//		int 타입의 변수를 3개 선언하고
+//		세 정수 중에서 최대값, 최소값을 구하는 프로그램을 작성하시오
+		
+//		System.out.println("최대값은 : " + a);
+//		System.out.println("최소값은 : " + a);
+		
+		int a = 2;
+		int b = 2;
+		int c = 1;
+		
+		if(a != b && b != c && c != a) {
+			if(a > b && a > c) {
+				System.out.println("최대값은 : " + a);
+			} else if (b > a && b > c) {
+				System.out.println("최대값은 : " + b);
+			} else {
+				System.out.println("최대값은 : " + c);
+			}
+			
+			if(a < b && a < c) {
+				System.out.println("최소값은 : " + a);
+			} else if ( b < a && b < c ) {
+				System.out.println("최소값은 : " + b);
+			} else {
+				System.out.println("최소값은 : " + c);
+			}
+		} else {
+			System.out.println("적어도 2개의 동일한 정수가 존재 합니다. 수정해 주세요");
+		}
+
+		
+	}
+
+}
