685a6140fd98aa25a80eca481acc2c4c1ca5a787
hyobin-park <qlswkd03@gmail.com>
2024-08-05 17:27:38 +0900

    suv_new

--- src/com/mycompany/ch06/first/Suv.java
+++ src/com/mycompany/ch06/first/Suv.java
@@ -21,4 +21,39 @@ public class Suv {
 		return result;
 	}
 	
+
+//			인풋: 문자열(사장,부장,과장,대리,사원,나)
+//			직급에 따른 연봉을 알려주는 함수
+		
+		public int salary(String position) {
+			int salary = 0;
+			switch(position) {
+			case "사장": {
+				salary = 150000000;
+				break;
+			}
+			case "부장": {
+				salary = 100000000;
+				break;
+			}
+			case "과장": {
+				salary = 80000000;
+				break;
+			}
+			case "대리": {
+				salary = 55000000;
+				break;
+			}
+			case "사원": {
+				salary = 45000000;
+				break;
+			}
+			case "나": {
+				salary =43000000;
+				break;
+			}
+			}
+			return salary;
+		}
+	
 }
--- src/com/mycompany/ch06/first/SuvEx.java
+++ src/com/mycompany/ch06/first/SuvEx.java
@@ -15,11 +15,18 @@ public class SuvEx {
 		suv.go();
 		suv.back();
 		
-		
 		int sum2 = suv.sum(10, 15);
 		System.out.println(sum2);
+		System.out.println("---------------------");
+//		System.out.println(suv.sum(10, 15)); 로도 표기 가능
 		
-//		System.out.println(suv.sum(10, 15));
+	
+//		인풋: 문자열(사장,부장,과장,대리,사원,나)
+//		직급에 따른 연봉을 알려주는 함수
+		
+		int result = suv.salary("나");
+		System.out.println("나: " + result);
+		
 		
 	}
 
